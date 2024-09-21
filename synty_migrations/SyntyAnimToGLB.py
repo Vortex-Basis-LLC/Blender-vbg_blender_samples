9c6cfc4208eff644a264bee50c9b031e82b29209
Geoffroy Warin <code@geowarin.com>
2024-09-22 00:31:06 +0200

    be smarter about which animation should loop

--- synty_migrations/SyntyAnimToGLB.py
+++ synty_migrations/SyntyAnimToGLB.py
@@ -1,13 +1,12 @@
 import glob
 import math
+import time
 
 import bpy
 import os
-import time
 import mathutils
 from typing import Generator, List, Tuple
 from attr import dataclass
-
 
 # Purpose: This script is intended to take the Synty Locomotion and Sword animation packages and re-map them onto a character with a T-Pose.
 #   But, it could probably generally be used for remapping animations between armatures with same bone names and approximate general proportions.
@@ -46,18 +45,19 @@ def sanity_check_project_is_as_intended():
     if target_armature is None:
         print("Object for target_armature_name not found:", target_armature_name)
         return False
-    
+
     for obj in bpy.data.objects:
         # We assume we won't care about top-level armatures that are not the target armature.
         if obj.parent is None:
             if obj != target_armature:
-                print("Target armature should be only top-level object in the project. You probably don't want to run this script here. It will mostly delete everything else except target armature when done.")
+                print(
+                    "Target armature should be only top-level object in the project. You probably don't want to run this script here. It will mostly delete everything else except target armature when done.")
                 return False
-    
-    if not os.path.exists(export_path): 
+
+    if not os.path.exists(export_path):
         print("Export path does not exist:", export_path)
         return False
-                    
+
     return True
 
 
@@ -94,7 +94,7 @@ def remove_all_copy_transforms_bone_constraints_on_armature(target_armature):
 def add_copy_transforms_constraints_to_all_bones(source_armature, target_armature, offsets):
     for target_bone in target_armature.pose.bones:
         if target_bone.name in source_armature.pose.bones:
-            
+
             loc_constraint = target_bone.constraints.new('COPY_LOCATION')
             loc_constraint.target = source_armature
             loc_constraint.subtarget = target_bone.name
@@ -102,7 +102,6 @@ def add_copy_transforms_constraints_to_all_bones(source_armature, target_armatur
             loc_constraint.owner_space = 'LOCAL'
             loc_constraint.use_offset = True
             loc_constraint.influence = 1.0
-            
 
             rot_constraint = target_bone.constraints.new('COPY_ROTATION')
             rot_constraint.target = source_armature
@@ -110,9 +109,9 @@ def add_copy_transforms_constraints_to_all_bones(source_armature, target_armatur
             # rot_constraint.target_space = 'LOCAL'
             # rot_constraint.owner_space = 'LOCAL'
             rot_constraint.influence = 1.0
-    
+
             if target_bone.name == "Root":
-                target_bone.matrix_basis.translation -= offsets["Hips"]     
+                target_bone.matrix_basis.translation -= offsets["Hips"]
                 print(f"{target_bone.name} final location: {target_bone.matrix_basis.translation}")
                 bpy.context.view_layer.update()
 
@@ -125,7 +124,7 @@ def apply_animation_constraints_per_frame(target_armature, frame_from, frame_to)
     ks = bpy.data.scenes["Scene"].keying_sets_all
     ks.active = ks['Location & Rotation']
     scene = bpy.context.scene
-    for frame in range(frame_from, frame_to + 1):    
+    for frame in range(frame_from, frame_to + 1):
         scene.frame_set(frame)
         # Apply transforms for this frame.
         bpy.ops.pose.visual_transform_apply()
@@ -179,6 +178,7 @@ def rotate_keyframe_points(target_action, target_path, angle, axis='Y'):
             target_handle_right[0] = target_co[0] + 0.4
             target_handle_right[1] = target_co[1]
 
+
 def push_armature_action_to_new_nla_strip(armature, frame_from, track_name, strip_name):
     if armature.animation_data is not None:
         action = armature.animation_data.action
@@ -198,10 +198,10 @@ def delete_blender_object(obj_to_delete):
     obj_to_delete.select_set(True)
     bpy.ops.object.delete()
 
+
 @dataclass
 class ExportSet:
     export_name: str
-    loop: bool = True
     fbxs: List[str] = []
 
 
@@ -216,6 +216,7 @@ def clear_and_reset_armature(armature):
     bpy.ops.object.mode_set(mode='OBJECT')
     bpy.context.view_layer.update()
 
+
 def do_custom_animation_fixups(target_armature):
     # Handle special tweaking for animations here.
     if fixup_synty_third_finger:
@@ -224,13 +225,14 @@ def do_custom_animation_fixups(target_armature):
             target_armature.animation_data.action,
             'pose.bones["Finger_03"].rotation_quaternion',
             math.pi,
-            axis = 'Y')
+            axis='Y')
 
         rotate_keyframe_points(
             target_armature.animation_data.action,
             'pose.bones["Finger_04"].rotation_quaternion',
             -math.pi / 2.0,
-            axis = 'Z')
+            axis='Z')
+
 
 def do_animation_imports_exports_main(export_set: ExportSet):
     export_name = export_set.export_name
@@ -238,12 +240,13 @@ def do_animation_imports_exports_main(export_set: ExportSet):
     delete_all_nla_tracks_on_armature(target_armature)
     clear_animation_action_on_armature(target_armature)
     remove_all_unused_animation_actions_in_file()
-    
+
     print("Export Name:", export_name)
     for fbx_path in export_set.fbxs:
-        process_fbx(export_set, fbx_path, target_armature)
+        process_fbx(fbx_path, target_armature)
         gltf_export(export_name)
-        
+
+
 def calculate_initial_offsets(source_armature, target_armature):
     offsets = {}
     for bone_name in target_armature.pose.bones.keys():
@@ -263,45 +266,51 @@ def calculate_initial_offsets(source_armature, target_armature):
 
             print(f"Offset for {bone_name} calculated: {offset}")
     return offsets
-    
-def process_fbx(export_set: ExportSet, fbx_path, target_armature):
+
+
+def process_fbx(fbx_path, target_armature):
     base_file_name = os.path.splitext(os.path.basename(fbx_path))[0]
     print("FILE: ", fbx_path, " ", base_file_name)
-    
+
     remove_all_unused_animation_actions_in_file()
-    
+
     print("Target armature position:", target_armature.location)
-    
+
     bpy.ops.import_scene.fbx(filepath=fbx_path, automatic_bone_orientation=True)
     source_armature_list = get_source_armatures(target_armature)
-    
+
     for source_armature in source_armature_list:
         if source_armature.animation_data is None or source_armature.animation_data.action is None:
             continue
-        
+
         print("Source armature position:", source_armature.location)
         source_action = source_armature.animation_data.action
         frame_from = int(source_action.frame_range[0])
         frame_to = int(source_action.frame_range[1])
         print("Copying Animation:", source_armature.name, "; Frames:", frame_from, "to", frame_to)
 
-
         remove_all_copy_transforms_bone_constraints_on_armature(target_armature)
-        add_copy_transforms_constraints_to_all_bones(source_armature, target_armature, calculate_initial_offsets(source_armature, target_armature))
+        add_copy_transforms_constraints_to_all_bones(source_armature, target_armature,
+                                                     calculate_initial_offsets(source_armature, target_armature))
         apply_animation_constraints_per_frame(target_armature, frame_from, frame_to)
         remove_all_copy_transforms_bone_constraints_on_armature(target_armature)
         do_custom_animation_fixups(target_armature)
 
         anim_name = base_file_name
-        if export_set.loop:
+        if should_loop(anim_name):
             anim_name = anim_name + '-loop'
         push_armature_action_to_new_nla_strip(target_armature, frame_from, anim_name, anim_name)
 
         print("Removing:", source_armature.name)
         delete_blender_object(source_armature)
-    
+
     remove_all_unused_animation_actions_in_file()
     clear_and_reset_armature(target_armature)
+
+
+def should_loop(anim_name: str):
+    return (any(word in anim_name.lower() for word in {"idle", "crouch", "walk", "strafe", "fall", "run", "loop", "turn"}) 
+            and all(word not in anim_name.lower() for word in {"hit", "begin", "end", "_to", "_returnto", "jump", "attack"}))
 
 def gltf_export(export_name):
     full_export_path = os.path.join(export_path, export_name + '.glb')
@@ -322,17 +331,17 @@ def get_source_armatures(target_armature):
     return source_armature_list
 
 
-def get_fbx_dirs(folder_path: str, name: str, loop: bool = False) -> Generator[ExportSet, None, None]:
+def get_fbx_dirs(folder_path: str, name: str) -> Generator[ExportSet, None, None]:
     for dir_name in os.listdir(folder_path):
         full_path = os.path.join(folder_path, dir_name)
         if os.path.isdir(full_path):
             regular_fbx, root_motion_fbx = filter_fbx_files(full_path)
             output_name = f"{name}_{dir_name}"
             if regular_fbx:
-                yield ExportSet(output_name, loop, regular_fbx)
+                yield ExportSet(output_name, regular_fbx)
             if root_motion_fbx:
-                yield ExportSet(f"{output_name}_root", loop, root_motion_fbx)
-            yield from get_fbx_dirs(full_path, f"{name}_{dir_name.lower()}", loop)
+                yield ExportSet(f"{output_name}_root", root_motion_fbx)
+            yield from get_fbx_dirs(full_path, f"{name}_{dir_name.lower()}")
 
 
 def filter_fbx_files(directory: str) -> Tuple[List[str], List[str]]:
